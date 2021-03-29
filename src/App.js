d961a1263516cb0f771c197f33edd86bfb58bade
Amine <amineprojet7@gmail.com>
2021-03-29 15:01:46 +0200

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/alt-text */
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="logo">
+      <img src="https://www.elise.com.fr/assets/templates/meridian/images/logo.jpg" />
     </div>
+    <div className="App">
+      <div className="inputs">
+        <div className="form">
+          <i class="fas fa-user icon"></i>
+          <input className="control" type="text" placeholder="Utilisateur" />
+        </div>
+        <br />
+        <div className="form">
+          <i class="fas fa-user icon"></i>
+          <input className="control" type="text" placeholder="Mot de passe" />
+        </div>
+        <br />
+        <div className="submit">
+          <button className="login" type="submit">Connexion</button>
+        </div>
+      </div>
+    </div>
+    </>
   );
 }
 
