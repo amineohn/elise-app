c85a8f94ae4df5b90b80e1ba9ec1340582db992f
Amine <amineprojet7@gmail.com>
2021-05-04 15:31:08 +0200

    loader on logo.

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,15 +1,42 @@
 /* eslint-disable jsx-a11y/alt-text */
 import { Link } from "react-router-dom";
+
 import logo from "../assets/img/logo.jpg";
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
+const LoadingLogo = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const cacheImages = async (srcArray) => {
+    const promises = await srcArray.map((src) => {
+      return new Promise(function (resolve, reject) {
+        const img = new Image()
+
+        img.src = src;
+        img.onload = resolve()
+        img.onerror = reject()
+      })
+    })
+      
+    await Promise.all(promises)
+
+    setIsLoading(false)
+  }
+  useEffect(() => {
+    const img = [logo];
+    cacheImages(img);
+  })
+  return (
+    <div className="logo moveFromTop">
+      {isLoading ?
+       'Chargement..' : <Link to="/">
+          <img src={logo} />
+        </Link>}
+    </div>
+  )
+}
 class Logo extends Component {
   render() {
     return (
-      <div className="logo moveFromTop">
-        <Link to="/">
-          <img src={logo} />
-        </Link>
-      </div>
+      <LoadingLogo />
     )
   }
 }
