022bb98065ae38faa79eb737d980c8788ada5fdf
imveny <amineprojet7@gmail.com>
2021-06-23 11:15:23 +0200

    yeet

--- src/components/home/AddWeight.jsx
+++ src/components/home/AddWeight.jsx
@@ -1,39 +1,26 @@
 import React, { Component } from "react";
 import Icons from "../Icons";
 import { useUser } from "@auth0/nextjs-auth0";
-export default class AddWeight extends Component {
-  render() {
-    const { user, error, isLoading } = useUser();
-    if (error) return <div>{error.message}</div>;
-    return (
-      <div className="data">
-        {user ? (
-          <input
-            className="enter"
-            type="number"
-            placeholder="Saisir un poids"
-          />
-        ) : (
-          <input
-            className="enter"
-            type="number"
-            placeholder="Saisir un poids"
-            disabled
-          />
-        )}
-        &nbsp;
-        {user ? (
-          <button className="enter-submit poids gray">
-            <Icons icon="login" />
-            Ajouter
-          </button>
-        ) : (
-          <button className="enter-submit poids gray" disabled>
-            <Icons icon="login" />
-            Ajouter
-          </button>
-        )}
-      </div>
-    );
-  }
+export default function AddWeight() {
+  const { user, error, isLoading } = useUser();
+  if (error) return <div>{error.message}</div>;
+  return (
+    <div className="data">
+      {user ? (
+        <input className="enter" type="number" placeholder="Saisir un poids" />
+      ) : (
+        <input
+          className="enter"
+          type="number"
+          placeholder="Saisir un poids"
+          disabled
+        />
+      )}
+      &nbsp;
+      <button className="enter-submit poids gray" {...(user ? "" : "disabled")}>
+        <Icons icon="login" />
+        Ajouter
+      </button>
+    </div>
+  );
 }
