020eb66c5e4a06a87113b73f49b52a6ba238d080
imveny <amineprojet7@gmail.com>
2021-06-23 11:04:33 +0200

    block add weight wihout logged

--- src/components/home/AddWeight.jsx
+++ src/components/home/AddWeight.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
 import Icons from "../Icons";
+import { useUser } from "@auth0/nextjs-auth0";
 export default class AddWeight extends Component {
   render() {
+    const { user, error, isLoading } = useUser();
+    if (error) return <div>{error.message}</div>;
     return (
       <div className="data">
-        <input className="enter" type="number" placeholder="Saisir un poids" />
+        {user ? (
+          <input
+            className="enter"
+            type="number"
+            placeholder="Saisir un poids"
+          />
+        ) : (
+          <input
+            className="enter"
+            type="number"
+            placeholder="Saisir un poids"
+            disabled
+          />
+        )}
         &nbsp;
-        <button className="enter-submit poids gray">
-          <Icons icon="login" />
-          Ajouter
-        </button>
+        {user ? (
+          <button className="enter-submit poids gray">
+            <Icons icon="login" />
+            Ajouter
+          </button>
+        ) : (
+          <button className="enter-submit poids gray" disabled>
+            <Icons icon="login" />
+            Ajouter
+          </button>
+        )}
       </div>
     );
   }
