5dd76eaf5664776a7a0866be20360d790b395ff2
amineohn <amineprojet7@gmail.com>
2021-10-08 15:05:12 +0200

    way better idea.

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -38,7 +38,7 @@ const Table = () => {
                                     <td
                                         id={item.id}
                                         key={item.id}
-                                        className="px-4 py-3 rounded-2xl"
+                                        className="px-4 py-3 rounded-2xl cursor-pointer"
                                         onClick={handleDelete}
                                     >
                                         {item.type}
@@ -46,7 +46,7 @@ const Table = () => {
                                     <td
                                         id={item.id}
                                         key={item.id}
-                                        className="px-4 py-3 rounded-2xl"
+                                        className="px-4 py-3 rounded-2xl cursor-pointer"
                                         onClick={handleDelete}
                                     >
                                         {item.matter}
@@ -54,33 +54,21 @@ const Table = () => {
                                     <td
                                         id={item.id}
                                         key={item.id}
-                                        className="px-4 py-3 rounded-2xl"
+                                        className="px-4 py-3 rounded-2xl cursor-pointer"
                                         onClick={handleDelete}
                                     >
                                         {item.weight}
                                         <span className="text-xs">kg</span>
                                     </td>
-                                    <button
-                                        onClick={handleDelete}
-                                        aria-label="Submit"
-                                        type="submit"
+                                    <div
+                                        id={item.id}
+                                        key={item.id}
+                                        className=" rounded-2xl bg-red-500 text-white transition duration-110 w-6"
                                     >
-                                        <svg
-                                            className="w-5 h-5 fill-current text-red-500"
-                                            aria-hidden="true"
-                                            focusable="false"
-                                            data-prefix="fas"
-                                            data-icon="times-circle"
-                                            role="img"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 512 512"
-                                        >
-                                            <path
-                                                fill="currentColor"
-                                                d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"
-                                            ></path>
-                                        </svg>
-                                    </button>
+                                        <span className="ml-2 text-center text-medium">
+                                            x
+                                        </span>
+                                    </div>
                                 </div>
                             </tr>
                         </FadeIn>
