14ef18aab0d5dd14eb20138fdace12d53508bdc7
amineohn <amineprojet7@gmail.com>
2021-10-29 08:24:46 +0200

    Next 12.0

--- next.config.js
+++ next.config.js
@@ -1,29 +1,3 @@
 module.exports = {
-    webpack5: false,
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-            use: {
-                loader: 'url-loader',
-                options: {
-                    limit: 100000,
-                    name: '[name].[ext]',
-                },
-            },
-        })
-
-        config.node = {
-            dns: 'mock',
-            fs: 'empty',
-            net: 'empty',
-            tls: 'empty',
-            path: true,
-            url: false,
-        }
-
-        return config
-    },
-    images: {
-        disableStaticImages: true,
-    },
+    webpack5: true,
 }
